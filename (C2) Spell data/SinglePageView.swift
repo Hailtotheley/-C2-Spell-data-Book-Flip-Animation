bbe7a064e4a5af4f1fefdc9931bf9ad834fc7a8f
Hailey <tanzhailey@gmail.com>
2025-08-03 09:20:57 +0800

    added SinglePageView

---
+++ (C2) Spell data/SinglePageView.swift
@@ -0,0 +1,18 @@
+//
+//  SinglePageView.swift
+//  (C2) Spell data
+//
+//  Created by Eugene Tan on 3/8/25.
+//
+
+import SwiftUI
+
+struct SinglePageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SinglePageView()
+}
